f2724d9ee94cdf25cbbba0c0481126987ff967c7
AmteurPotion <1k0j2i4@gmail.com>
2021-05-10 18:03:04 +0900

    빌드 커맨드 제거

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-del D:\SteamLibrary\steamapps\common\Mindustry\saves\mods\TestDesktop.jar
-xcopy D:\workspace\Java\Test\build\libs\TestDesktop.jar D:\SteamLibrary\steamapps\common\Mindustry\saves\mods\ /k
-del D:\workspace\Java\Test\build\libs\TestDesktop.jar
+del D:\SteamLibrary\steamapps\common\Mindustry\saves\mods\CoreDesktop.jar
+xcopy D:\workspace\Java\Core\build\libs\CoreDesktop.jar D:\SteamLibrary\steamapps\common\Mindustry\saves\mods\ /k
+del D:\workspace\Java\Core\build\libs\CoreDesktop.jar
 start /max steam://rungameid/1127400
 : java -jar D:\SteamLibrary\steamapps\common\Mindustry\jre\desktop.jar
 
