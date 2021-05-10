128ddb8837b56ddb211555e4de2d3f12bbc0f44a
AmteurPotion <1k0j2i4@gmail.com>
2021-05-10 18:06:11 +0900

    필요없는 임포트 삭제

--- src/core/Vars.java
+++ src/core/Vars.java
@@ -2,11 +2,7 @@ package core;
 
 import static mindustry.Vars.*;
 
-import arc.scene.ui.layout.Table;
-import arc.struct.Seq;
 import core.ui.UICollection;
-import core.ui.fragments.*;
-import core.ui.layouts.Layout;
 
 public class Vars {
     public static final UICollection uic = new UICollection();
