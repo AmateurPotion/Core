3daa2e6deaa82ab1d3ca272cc4a5fd9c5b3d6485
AmteurPotion <1k0j2i4@gmail.com>
2021-05-11 17:13:57 +0900

    디버그모드 추가

--- src/core/Vars.java
+++ src/core/Vars.java
@@ -5,6 +5,7 @@ import static mindustry.Vars.*;
 import core.ui.UICollection;
 
 public class Vars {
+    public static boolean debugMode = false;
     public static final UICollection uic = new UICollection();
     public Vars() {
 
