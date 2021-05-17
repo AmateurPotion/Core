7803a879deef3e76e421c9233efb698f88ffd479
AmteurPotion <1k0j2i4@gmail.com>
2021-05-17 20:18:14 +0900

    쓸모 없는 메소드 삭제 및 스레드 업데이트 방식 변경, 변수 접근제어자 변경

--- src/core/ui/UICollection.java
+++ src/core/ui/UICollection.java
@@ -12,9 +12,12 @@ import java.util.Objects;
 import static core.Vars.*;
 
 public final class UICollection {
-    private final Seq<Layout> layoutList;
-    public boolean update = true;
-    private boolean threadRunning = false;
+    public final Seq<Layout> layoutList;
+    private boolean init = true;
+    public boolean updateOnce = true;
+    public boolean updateTick = false;
+    private Thread layoutDrawer;
+
     public UICollection() {
         layoutList = Seq.with();
     }
@@ -31,7 +34,7 @@ public final class UICollection {
     public void addLayout(Layout layout) {
         if(layoutList.find(cont -> Objects.equals(cont.id, layout.id)) == null){
             layoutList.add(layout);
-            update = true;
+            updateOnce = true;
         } else if(debugMode) {
             Log.info(layout.id + " is not active because it already exists.");
         }
@@ -49,21 +52,9 @@ public final class UICollection {
         return layoutList.find(layout -> Objects.equals(layout.id, id)) != null ? layoutList.find(layout -> Objects.equals(layout.id, id)) : new Layout(id, cont ->{}, 0);
     }
 
-    public void setLayoutVisible(String id, boolean visible) {
-        layoutList.find(layout -> Objects.equals(layout.id, id)).visible = visible;
-        if(layoutList.find(layout -> Objects.equals(layout.id, id)).visible != visible) {
-            update = true;
-        }
-    }
-
-    public void delLayout(String id) {
-        layoutList.removeAll(layout -> Objects.equals(layout.id, id));
-        update = true;
-    }
-
     public void toggleLayout(String layoutId) {
         uic.layoutList.find(layout -> Objects.equals(layout.id, layoutId)).visible = !uic.layoutList.find(layout -> Objects.equals(layout.id, layoutId)).visible;
-        update = true;
+        updateOnce = true;
     }
 
     private class FragmentDialog extends Fragment {
@@ -75,16 +66,27 @@ public final class UICollection {
                 cont.table(Tex.buttonTrans, table -> {
                     cont.visible(() -> layoutList.filter(layout -> layout.visible).size > 0);
                     cont.update(() -> {
-                        if (update && !threadRunning) {
-                            new Thread(() -> {
-                                table.clear();
-                                layoutList.sort(layout -> layout.priority);
-                                for (Layout layout : layoutList.toArray()) {
-                                    if (layout.visible) layout.content.get(table);
+                        if (init) {
+                            init = false;
+                            layoutDrawer = new Thread(() -> {
+                                for(;;) {
+                                    if(updateOnce || updateTick) {
+                                        table.clear();
+                                        layoutList.sort(layout -> layout.priority);
+                                        for (Layout layout : layoutList.toArray()) {
+                                            if (layout.visible) layout.content.get(table);
+                                        }
+                                        updateOnce = false;
+                                    }
+                                    try {
+                                        Thread.sleep(300);
+                                    } catch (InterruptedException e) {
+                                        Log.errTag("Layout error", e.getLocalizedMessage());
+                                    }
                                 }
-                                threadRunning = false;
-                            }).start();
-                            update = false;
+                            });
+                            layoutDrawer.setName("Layout Drawer");
+                            layoutDrawer.start();
                         }
                     });
                 }).margin(14f).minWidth(360f);
