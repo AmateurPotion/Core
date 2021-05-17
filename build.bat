e40713e5293f99686063a53c6fef8095ece1ad5d
AmateurPotion <47741752+AmateurPotion@users.noreply.github.com>
2021-05-17 20:10:09 +0900

    Delete build.bat
    로컬전용 파일 삭제

--- build.bat
+++
@@ -1,6 +0,0 @@
-del D:\SteamLibrary\steamapps\common\Mindustry\saves\mods\CoreDesktop.jar
-xcopy D:\workspace\Java\Core\build\libs\CoreDesktop.jar D:\SteamLibrary\steamapps\common\Mindustry\saves\mods\ /k
-del D:\workspace\Java\Core\build\libs\CoreDesktop.jar
-start /max steam://rungameid/1127400
-: java -jar D:\SteamLibrary\steamapps\common\Mindustry\jre\desktop.jar
-
