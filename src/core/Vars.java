2e40b9d68d08bb57fd3e761fa912e51eb3adc657
AmteurPotion <1k0j2i4@gmail.com>
2021-06-16 11:29:46 +0900

    기초 설정 추가

--- src/core/Vars.java
+++ src/core/Vars.java
@@ -6,6 +6,7 @@ import core.ui.UICollection;
 
 public class Vars {
     public static boolean debugMode = false;
+    public static int craftDefaultStack = 10;
     public static final UICollection uic = new UICollection();
     public Vars() {
 
