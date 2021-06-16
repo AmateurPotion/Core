f874274e52b0e9565e77a42fc82e96600f3106d4
AmteurPotion <1k0j2i4@gmail.com>
2021-06-16 11:40:33 +0900

    빈 아이템

--- src/core/cotents/ItemList.java
+++ src/core/cotents/ItemList.java
@@ -1,10 +1,18 @@
 package core.cotents;
 
+import arc.graphics.Color;
 import mindustry.ctype.ContentList;
+import mindustry.type.Item;
 
 public class ItemList implements ContentList {
+    public static Item blank;
     @Override
     public void load() {
-
+        blank = new Item("blank", Color.valueOf("ff0000")){{
+            hardness = 0;
+            flammability = 10f;
+            explosiveness = 10f;
+            cost = 100f;
+        }};
     }
 }
