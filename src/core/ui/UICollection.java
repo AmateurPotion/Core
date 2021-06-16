b4cf6dce9e591811b09ed86a9c6e2b803ad3023f
AmteurPotion <1k0j2i4@gmail.com>
2021-06-16 11:40:04 +0900

    불안정한 변수들 큐로 변경

--- src/core/ui/UICollection.java
+++ src/core/ui/UICollection.java
@@ -2,37 +2,41 @@ package core.ui;
 
 import arc.func.Cons;
 import arc.scene.Group;
+import arc.scene.event.Touchable;
+import arc.scene.ui.layout.Cell;
 import arc.scene.ui.layout.Table;
-import arc.util.Log;
 import mindustry.ui.fragments.Fragment;
 
+import java.util.LinkedList;
 import java.util.Objects;
+import java.util.Queue;
 
 public final class UICollection {
     private boolean update = true;
-    private static int updateState = 0;
-    private static Table temp;
-    private String tempName;
+    private final Queue<Integer> updateState = new LinkedList<>();
+    private final Queue<Table> tables = new LinkedList<>();
+    private final Queue<String> names = new LinkedList<>();
     private final FragmentDialog layoutList = new FragmentDialog();
     public void build(Group parent) {
         layoutList.build(parent);
     }
 
     public void addChild(String name, Cons<Table> content) {
-        temp = new Table(content);
+        Table temp = new Table(content);
         temp.name = name;
-        updateState = 1;
+        tables.add(temp);
+        updateState.add(1);
         update = true;
     }
 
     public void removeChild(String name) {
-        tempName = name;
-        updateState = 2;
+        names.add(name);
+        updateState.add(2);
         update = true;
     }
     public void toggle(String name) {
-        tempName = name;
-        updateState = 3;
+        names.add(name);
+        updateState.add(3);
         update = true;
     }
 
@@ -41,24 +45,25 @@ public final class UICollection {
         @Override
         public void build(Group parent) {
             parent.fill(cont -> cont.update(() -> {
-                if(update) {
-                    Log.info("adasda");
-                    if(updateState == 1) {
-                        Log.info("aaaa");
-                        if(!cont.getChildren().contains(child -> Objects.equals(child.name, temp.name))) cont.add(temp);
-                        temp = new Table(c -> c.name = "null");
-                    }
-                    switch (updateState) {
+                String t;
+                Table tempTable;
+                if(updateState.size() > 0) {
+                    switch (updateState.poll()) {
+                        case 1:
+                            tempTable = tables.poll();
+                            if(!cont.getChildren().contains(child -> Objects.equals(child.name, tempTable != null ? tempTable.name : null))) cont.add(tempTable);
+                            break;
                         case 2:
-                            if(cont.getChildren().find(child -> Objects.equals(child.name, tempName)) != null) cont.getChildren().find(child -> Objects.equals(child.name, tempName)).remove();
+                            t = names.poll();
+                            if(cont.getChildren().find(child -> Objects.equals(child.name, t)) != null) cont.getChildren().find(child -> Objects.equals(child.name, t)).remove();
                             break;
                         case 3:
-                            if(cont.getChildren().find(child -> Objects.equals(child.name, tempName)) != null) cont.getChildren().find(child -> Objects.equals(child.name, tempName)).visible = !cont.getChildren().find(child -> Objects.equals(child.name, tempName)).visible;
+                            t = names.poll();
+                            if(cont.getChildren().find(child -> Objects.equals(child.name, t)) != null) cont.getChildren().find(child -> Objects.equals(child.name, t)).visible = !cont.getChildren().find(child -> Objects.equals(child.name, t)).visible;
                             break;
                         default:
                             break;
                     }
-                    updateState = 0;
                     update = false;
                 }
             }));
