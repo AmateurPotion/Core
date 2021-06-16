73a8e825b8471931de007f24ce87cc92398a8675
AmteurPotion <1k0j2i4@gmail.com>
2021-06-16 11:45:31 +0900

    프레임 건설 블럭 자동 생성 구상중

---
+++ src/core/world/blocks/frame/CustomizableBlock.java
@@ -0,0 +1,9 @@
+package core.world.blocks.frame;
+
+import mindustry.world.Block;
+
+public class CustomizableBlock extends Block {
+    public CustomizableBlock(String name) {
+        super(name);
+    }
+}
