4fe4b7ac3cefa975b871d171755e29aa82a0a9e1
AmteurPotion <1k0j2i4@gmail.com>
2021-07-15 20:34:41 +0900

    오토타일 준비

--- src/core/world/blocks/autoTile/ItemPlate.java
+++ src/core/world/blocks/autoTile/ItemPlate.java
@@ -1,28 +1,58 @@
 package core.world.blocks.autoTile;
 
-import arc.graphics.g2d.Draw;
+import arc.Core;
+import arc.Events;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.struct.Seq;
+import arc.util.Log;
+import core.Variable;
+import core.ui.graphics.TextureEditor;
+import mindustry.game.EventType;
 import mindustry.type.Item;
 import mindustry.world.Tile;
-import mindustry.world.blocks.environment.Floor;
+import mindustry.world.blocks.environment.OverlayFloor;
 
-public class ItemPlate extends Floor {
+public class ItemPlate extends OverlayFloor {
     public final Item itemProperty;
     private final String shape;
+    public static final Seq<String> shapes = Seq.with("none", "ore");
 
     public ItemPlate(String name, Item property) {
-        this(name, property, "none");
+        this(name, property, shape(0));
     }
+
+    public ItemPlate(String name, Item property, int plateShape) {
+        this(name, property, shape(plateShape));
+    }
+
+    // 기본 판 모양(shape) 종류 : none, ore
     public ItemPlate(String name, Item property, String plateShape) {
         super(name);
         itemProperty = property;
-        shape = plateShape;
+        shape = shapes.contains(plateShape) ? plateShape : "none";
+        inEditor = true;
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+            PixmapRegion pr = Core.atlas.getPixmap("plate-" + shape);
+            this.uiIcon = new TextureRegion(new Texture(TextureEditor.paint(pr, itemProperty.color)));
+            Log.info(pr.width);
+            Log.info(uiIcon.width);
+            Log.info(variantRegions[1].width);
+        });
     }
 
     @Override
     public void drawBase(Tile tile){
-        Draw.color(itemProperty.color);
-        Draw.rect("plate-" + shape, tile.worldx(), tile.worldy());
-        Draw.color();
+        if(tile.build != null){
+            tile.build.draw();
+        } else {
+            Draw.color(itemProperty.color);
+            Draw.rect(Variable.modName + "-plate-" + shape, tile.drawx(), tile.drawy());
+            Draw.color();
+        }
     }
 
+    public static String shape(int shapeId) {
+        return shapeId >= shapes.size ? "none" : shapes.get(shapeId);
+    }
 }
