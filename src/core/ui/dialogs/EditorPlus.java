c6d829ab81c3915c66d44eba4ef409294b9fb93d
AmteurPotion <1k0j2i4@gmail.com>
2021-07-30 20:01:49 +0900

    EditorPlus

--- src/core/ui/dialogs/EditorPlus.java
+++ src/core/ui/dialogs/EditorPlus.java
@@ -5,6 +5,7 @@ import arc.scene.ui.layout.Table;
 import arc.util.Log;
 import mindustry.Vars;
 import mindustry.game.EventType;
+import mindustry.gen.Call;
 import mindustry.world.Tile;
 import mindustry.world.Tiles;
 
@@ -38,7 +39,7 @@ public class EditorPlus {
             table.row();
             table.button("copy", () -> {
                 if(startPoint != null && endPoint != null) {
-                    int tw = endPoint.x - startPoint.x, th = endPoint.y - startPoint.y, sx = startPoint.x < endPoint.x ? startPoint.x : endPoint.x, sy = (startPoint.y < endPoint.y ? startPoint.y : endPoint.y);
+                    int tw = endPoint.x - startPoint.x, th = endPoint.y - startPoint.y, sx = Math.min(startPoint.x, endPoint.x), sy = Math.min(startPoint.y, endPoint.y);
                     copyData = new Tiles((tw > 0 ? tw : -tw) + 1, (th > 0 ? th : -th) + 1);
                     copyData.fill();
                     for(int y = 0; y < copyData.height; y++) {
@@ -62,6 +63,14 @@ public class EditorPlus {
                         endPoint = e.tile;
                         break;
                     case 3:
+                        if(copyData != null) {
+                            int sx = e.tile.x, sy = Math.max(e.tile.y - copyData.height + 1, 0);
+                            for(int y = 0; y < (e.tile.y - copyData.height > -2 ? copyData.height : e.tile.y + 1); y++) {
+                                for(int x = 0; x < (e.tile.x + copyData.width < Vars.world.width() ? copyData.width : Vars.world.width() - e.tile.x); x++) {
+                                    Vars.world.tiles.set(sx + x, sy + y, copyData.get(x, y));
+                                }
+                            }
+                        }
                         break;
                 }
                 copyState = 0;
